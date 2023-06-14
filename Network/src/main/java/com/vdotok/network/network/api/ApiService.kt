34e189521455835708e161fef8ba8f4d66092f85
khizzar.mansoor@norgic.com <khizzar.mansoor@norgic.com>
2023-06-14 15:48:26 +0500

    fixed api url error

--- Network/src/main/java/com/vdotok/network/network/api/ApiService.kt
+++ Network/src/main/java/com/vdotok/network/network/api/ApiService.kt
@@ -8,36 +8,36 @@ import okhttp3.RequestBody
 import retrofit2.Response
 import retrofit2.http.*
 
-interface APIService {
+interface ApiService {
 
-    @POST("/API/v0/Login")
+    @POST("Login")
     suspend fun loginUser(@Body model: LoginUserModel): LoginResponse
 
-    @POST("/API/v0/SignUp")
+    @POST("SignUp")
     suspend fun signUp(@Body model: SignUpModel): LoginResponse
 
-    @POST("/API/v0/CheckUsername")
+    @POST("CheckUsername")
     suspend fun checkUserName(@Body model: CheckUserModel): Response<LoginResponse>
 
-    @POST("/API/v0/CheckEmail")
+    @POST("CheckEmail")
     suspend fun checkEmail(@Body model: CheckUserModel): LoginResponse
 
-    @POST("/API/v0/AllUsers")
+    @POST("AllUsers")
     suspend fun getAllUsers(@Header("Authorization") auth_token: String): GetAllUsersResponseModel
 
-    @POST("/API/v0/AllGroups")
+    @POST("AllGroups")
     suspend fun getAllGroups(@Header("Authorization") auth_token: String): AllGroupsResponse
 
-    @POST("/API/v0/DeleteGroup")
+    @POST("DeleteGroup")
     suspend fun deleteGroup(@Header("Authorization") auth_token: String, @Body model: DeleteGroupModel): DeleteGroupResponseModel
 
-    @POST("/API/v0/RenameGroup")
+    @POST("RenameGroup")
     suspend fun updateGroupName(@Header("Authorization") auth_token: String, @Body model: UpdateGroupNameModel): UpdateGroupNameResponseModel
 
-    @POST("/API/v0/CreateGroup")
+    @POST("CreateGroup")
     suspend fun createGroup(@Header("Authorization") auth_token: String, @Body model: CreateGroupModel): CreateGroupResponse
 
-    @POST("/API/v0/AuthenticateSDK")
+    @POST("AuthenticateSDK")
     suspend fun authSDK(@Body model: AuthenticationRequest): Response<AuthenticationResponse>
 
     @Multipart
